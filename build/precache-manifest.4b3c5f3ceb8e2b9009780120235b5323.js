d443be0625482576bd8fde1aeed5e50bd1f027d9
李洪辉 <yishui.lhh@raycloud.com>
2020-10-16 12:42:06 +0800

    lhh

---
+++ build/precache-manifest.4b3c5f3ceb8e2b9009780120235b5323.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "330396321befbbc979e97a7f47bdd41e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "771355aaf676586cfb3d",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5a46617966b536919ab2",
+    "url": "/static/js/2.68f56c50.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.68f56c50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "771355aaf676586cfb3d",
+    "url": "/static/js/main.ed0de294.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
